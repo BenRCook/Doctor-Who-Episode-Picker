915ba59f6922de2a4ea963617c143b3a045a0e7f
Ben <40598265+BenRCook@users.noreply.github.com>
2022-07-18 20:07:34 +0100

    outputs random series 1 episode to console

---
+++ picker.py
@@ -0,0 +1,18 @@
+import random
+
+episodes = ["S1E1 - Rose",
+            "S1E2 - The End of the World",
+            "S1E3 - The Unquiet Dead",
+            "S1E4 - Aliens of London",
+            "S1E5 - World War Three",
+            "S1E6 - Dalek",
+            "S1E7 - The Long Game",
+            "S1E8 - Father's Day",
+            "S1E9 - The Empty Child",
+            "S1E10 - The Doctor Dances",
+            "S1E11 - Boom Town",
+            "S1E12 - Bad Wolf",
+            "S1E13 - The Parting of the Ways",
+            ]
+
+print(random.choice(episodes))
